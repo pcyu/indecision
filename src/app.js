06a15f98e0eba3d8589e2c7c2b851c848eb018b7
Peter Yu <PeterYu@gmail.com>
2018-02-08 14:57:42 -0500

    indecision app default

--- src/app.js
+++ src/app.js
@@ -8,9 +8,7 @@ const app = {
 
 const onFormSubmit = (e) => {
   e.preventDefault();
-
   const option = e.target.elements.option.value;
-
   if (option) {
     app.options.push(option);
     e.target.elements.option.value = '';
@@ -18,16 +16,31 @@ const onFormSubmit = (e) => {
   }
 };
 
+const removeOptions = () => {
+  app.options = [];
+  renderOptions();
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
+const appRoot = document.getElementById('app');
+
 const renderOptions = () => {
   const template = (
     <div>
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{app.options && app.options.length > 0 ? "Here are your options" : "No options"} </p>
-      <p>{app.options.length}</p>
+      <button disabled= {app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={removeOptions}>Remove All</button>
       <ol>
-        <li>Item one</li>
-        <li>Item two</li>
+      {
+        app.options.map((option) => <li key={option}>Option: {option}</li>)
+      }
       </ol>
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option" />
@@ -35,9 +48,8 @@ const renderOptions = () => {
       </form>
     </div>
   );
-  const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+  ReactDOM.render(template, appRoot);
 }
 
 renderOptions();
