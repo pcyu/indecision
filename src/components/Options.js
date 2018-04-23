7eaac77f95d8e1b62f20156b50908b95434548e5
Peter Yu <PeterYu@gmail.com>
2018-04-23 17:21:52 -0400

    reverted back to two options needed to enable beginning the app

--- src/components/Options.js
+++ src/components/Options.js
@@ -11,7 +11,7 @@ const Options = (props) => (
         Remove All
       </button>
     </div>
-    {props.options.length === 0 && <p className="widget__message"> Please add two exercises to get started!</p>}
+    {props.options.length === 0 && <p className="widget__message"> Please add at least two exercises to get started!</p>}
     { 
       props.options.map((option, index) => (
         <Option 
