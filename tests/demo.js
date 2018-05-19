2b5f70302ff4fa9cf98f824533185c5372d05e50
arlia <peteryu@gmail.com>
2018-05-19 15:24:27 -0400

    removed nightwatch code

--- tests/demo.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'Demo test for Random Tabata' : function (client) {
-    client
-      .url('https://random-tabata.herokuapp.com/')
-      .waitForElementVisible('body', 1000)
-      .pause(5000)
-      .end();
-  }
-};
--- tests/demo2.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'Demo test for Random Tabata' : function (client) {
-    client
-      .url('https://random-tabata.herokuapp.com/')
-      .assert.title('Random Tabata')
-      .pause(5000)
-      .end();
-  }
-};
