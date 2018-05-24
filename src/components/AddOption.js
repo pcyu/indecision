de98de70c3acbf1f22fea3e49d732fc8003a48ab
arlia <peteryu@gmail.com>
2018-05-24 19:04:54 -0400

    error text appears above input now

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -17,11 +17,11 @@ export default class AddOption extends React.Component {
   render() {
     return (
       <div>
+        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
         <form className="add-option" onSubmit={this.handleAddOption}>
           <input className="add-option__input" type="text" name="option" autoComplete="off"/>
           <button className="button">Add Exercise</button>
         </form>
-        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
       </div>
     );
   }
